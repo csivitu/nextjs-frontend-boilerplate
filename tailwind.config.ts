7b3ddaf7ab26fe2139d7f6cd8100fdf9cfecb73d
manaslaud <manaslaud@hotmail.com>
2024-09-28 21:19:09 +0530

    feat:placeholders

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      //add other properties to in the extend object to add extra util classes to tailwind
+      //sample colors
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        slotBookDateColor: "#3E3E3E",
+        slotBookDateColorHover: "#1E1E1E",
+        slotBookTime: "#1E1E1E",
+        slotBookTimeGreen: "#30D158",
+        slotBookTimeRed: "#FF453A",
+      },
+      //custom breakpoints for better responsive design
+      screens: {
+        mobile: "320px",
+        tab: "768px",
+        laptopS: "1024px",
+        laptopM: "1440px",
       },
     },
   },
