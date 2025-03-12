3429d4c534e7b140b8b556e4d3d3a5b97cb327fc
Vibhor Agrawal <114611650+v1br@users.noreply.github.com>
2025-03-12 15:11:14 +0530

    feat: add pre-commit hook with husky (#99)
    * feat: add bun as package manager
    * feat: add husky with pre-commit hook
    * test: check husky pre-commit hook

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,33 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      //add other properties to in the extend object to add extra util classes to tailwind
-      //sample colors
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        slotBookDateColor: "#3E3E3E",
-        slotBookDateColorHover: "#1E1E1E",
-        slotBookTime: "#1E1E1E",
-        slotBookTimeGreen: "#30D158",
-        slotBookTimeRed: "#FF453A",
-      },
-      //custom breakpoints for better responsive design
-      screens: {
-        mobile: "320px",
-        tab: "768px",
-        laptopS: "1024px",
-        laptopM: "1440px",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			//add other properties to in the extend object to add extra util classes to tailwind
+			//sample colors
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				slotBookDateColor: "#3E3E3E",
+				slotBookDateColorHover: "#1E1E1E",
+				slotBookTime: "#1E1E1E",
+				slotBookTimeGreen: "#30D158",
+				slotBookTimeRed: "#FF453A",
+			},
+			//custom breakpoints for better responsive design
+			screens: {
+				mobile: "320px",
+				tab: "768px",
+				laptopS: "1024px",
+				laptopM: "1440px",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
